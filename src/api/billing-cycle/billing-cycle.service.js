02a247143970d1f9af4da972df8ced61ff5d07a5
Severo <severo.fabricio@gmail.com>
2018-04-15 19:05:28 -0300

    Adds error handler

--- src/api/billing-cycle/billing-cycle.service.js
+++ src/api/billing-cycle/billing-cycle.service.js
@@ -1,7 +1,10 @@
 const BillingCycle = require('./billing-cycle')
 
+const errorHandler = require('../common/error.handler')
+
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
   BillingCycle.count((error, value) => {
